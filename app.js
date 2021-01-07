3d899e4b106f52eedc0d45cad53e237b39820af8
Tommi Mäkelä <tmmi.makela@outlook.com>
2021-01-07 13:25:17 +0200

    Database connection added

--- app.js
+++ app.js
@@ -2,10 +2,17 @@ require('dotenv').config()
 const express = require('express')
 const app = express()
 //const cors = require('cors')
-//const mongoose = require('mongoose')
+const mongoose = require('mongoose')
 
-app.get('/', (request, response) => {
-  response.send('<h1>Hello World!</h1>')
-})
+// Opening a connection to the database
+console.log('connecting to MongoDB')
+mongoose.connect(process.env.MONGODB_URI, { useNewUrlParser: true, useUnifiedTopology: true, useFindAndModify: false, useCreateIndex: true })
+  .then(result => {
+    console.log('connected to MongoDB')
+  })
+  .catch((error) => {
+    console.log('error connecting to MongoDB:', error.message)
+  })
 
+mongoose.connection.close()
 module.exports = app
